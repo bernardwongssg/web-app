314cbf2c12f49df073774b8b9cfd627759b4e2da
bernard <productivity@Bernards-MacBook-Pro.local>
2023-11-05 15:43:24 -0800

    lesson 9

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -2,9 +2,23 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Profile
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField() # can set required to True or False
     
     class Meta: 
         model = User # when form.save() is called, it will be saved to 'User' model
         fields = ['username', 'email', 'password1', 'password2'] # password1 is password input, password2 is password validation
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField() # can set required to True or False
+    
+    class Meta: 
+        model = User # when form.save() is called, it will be saved to 'User' model
+        fields = ['username', 'email'] # password1 is password input, password2 is password validation
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta: 
+        model = Profile 
+        fields = ['image']
--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from PIL import Image
+
 # Create your models here.
 # creating profile with 1 to 1 relationship; 1 user can have 1 profile
 class Profile(models.Model):
@@ -9,3 +11,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self):
+        super().save() # parent's method save() will run (in this case, models)
+
+        img = Image.open(self.image.path)
+        print(self)
+        print('image URL: ' + str(self.image.url))
+        print('image Path: ' + str(self.image.path))
+
+        if img.height > 300 or img.width > 300: 
+            output_size = (300, 300)
+            img.thumbnail(output_size) # resizes image 
+            img.save(self.image.path) # saves the image into self's image path           
