c3f1ae1d7b43519badb4b9d233c76f4cf4ee1614
bernard <productivity@Bernards-MacBook-Pro.local>
2023-11-05 18:51:37 -0800

    lesson 10

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views 
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
-    path('about/', views.about, name = 'blog-about')
+    #path('', views.home, name = 'blog-home'),
+    path('', PostListView.as_view(), name = 'blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'), # pk is passed in so we know which post we're updating
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'), # pk is passed in so we know which post we're deleting
+    path('about/', views.about, name = 'blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse 
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 
 def home(request): 
     '''
@@ -24,5 +27,72 @@ def home(request):
     #return HttpResponse('<h1>Blog Home</h1>') 
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    # this will be looking for blog/post_detail.html
+    # items from this model will be referenced with the keyword 'object'
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # this will be looking for blog/post_form.html
+    # items from this model will be referenced with the keyword 'object'
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        '''
+        you do this before saving the form, b/c the author isn't set yet 
+        you can do this when validating the form. That's why you override form_valid()
+        instead of save() 
+        '''
+        # before you set the form, set the author 
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # this will be looking for blog/post_form.html
+    # items from this model will be referenced with the keyword 'object'
+    # NOTE: while the code is pretty similar to PostCreateView, you should add in extra validation to make sure the author is valid
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        '''
+        you do this before saving the form, b/c the author isn't set yet 
+        you can do this when validating the form. That's why you override form_valid()
+        instead of save() 
+        '''
+        # before you set the form, set the author 
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+
+    def test_func(self):
+        '''
+        used by UserPassesTestMixin
+        '''
+        post = self.get_object() # gets the post that we're currently trying to update 
+        return self.request.user == post.author # if current logged in user is equal to post author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        '''
+        used by UserPassesTestMixin
+        '''
+        post = self.get_object() # gets the post that we're currently trying to update 
+        return self.request.user == post.author # if current logged in user is equal to post author
+
+    
+
+    
+
 def about(request): 
     return render(request, 'blog/about.html', {'title':'About'})
+
